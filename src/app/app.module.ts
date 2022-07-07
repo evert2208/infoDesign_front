00854c5258b68562f4277bf330ac5ac71d544ce7
evert2208 <65311634+evert2208@users.noreply.github.com>
2022-07-07 13:29:15 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgChartsModule } from 'ng2-charts';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GraficaComponent } from './components/grafica/grafica.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GraficaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgChartsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
